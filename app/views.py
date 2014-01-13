4b1eebb4016574d7ebd629cdecf31733b1c9a697
anggao <ang.gao87@gmail.com>
2014-01-13 15:33:58 +0000

    add Jinja2 templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'nickname': 'Ang'} # fake user
+    posts = [ # fake array of posts
+              {
+                'author' : {'nickname' : 'Ang'},   
+                'body' : 'Beautiful day in Ireland!'
+              }, 
+              {
+                'author' : {'nickname' : 'Gao'},   
+                'body' : 'Hacking a blog'
+              } 
+            ]
+    return render_template('index.html', 
+            title = 'Home', 
+            user=user,
+            posts=posts)
