1ae44e544bc543858239ce5f2b8b058d5d7b912b
Ang Gao <ang.gao87@gmail.com>
2014-01-20 01:25:54 +0000

    blog post body full text search feature done

--- app/views.py
+++ app/views.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user,login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm, PostForm
+from forms import LoginForm, EditForm, PostForm, SearchForm
 from models import User, ROLE_ADMIN, ROLE_USER, Post
-from config import POSTS_PER_PAGE
+from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
@@ -82,12 +82,13 @@ def after_login(resp):
     return redirect(request.args.get('next') or url_for('index'))
 
 @app.before_request
-def before_qequest():
+def before_request():
     g.user = current_user
     if g.user.is_authenticated():
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
+        g.search_form = SearchForm()
 
 
 @app.route('/logout')
@@ -172,3 +173,18 @@ def unfollow(nickname):
     db.session.commit()
     flash('You have stopped following ' + nickname + '.')
     return redirect(url_for('user', nickname = nickname))
+
+@app.route('/search', methods=['post'])
+@login_required
+def search():
+    if not g.search_form.validate_on_submit():
+        return redirect(url_for('index'))
+    return redirect(url_for('search_results', query =
+            g.search_form.search.data))
+
+@app.route('/search_results/<query>')
+@login_required
+def search_results(query):
+    results = Post.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
+    return render_template('search_results.html', query = query, results =
+            results)
