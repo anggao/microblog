4354b3f277fffc09497b785679228c6be203cb2d
anggao <ang.gao87@gmail.com>
2014-02-20 18:10:29 +0000

    add test coverage

--- tests.py
+++ tests.py
@@ -9,6 +9,9 @@ from app import app, db
 from datetime import datetime, timedelta
 from app.models import User, Post
 from app.translate import microsoft_translate
+from coverage import coverage
+cov = coverage(branch = True, omit = ['flask/*', 'tests.py'])
+cov.start()
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -134,5 +137,40 @@ class TestCase(unittest.TestCase):
         db.session.delete(p)
         db.session.commit()
 
+    def test_user(self):
+        # make valid nicknames
+        n = User.make_valid_nickname('John_123')
+        assert n == 'John_123'
+        n = User.make_valid_nickname('John_[123]\n')
+        assert n == 'John_123'
+        # create a user
+        u = User(nickname = 'john', email = 'john@example.com')
+        db.session.add(u)
+        db.session.commit()
+        assert u.is_authenticated() == True
+        assert u.is_active() == True
+        assert u.is_anonymous() == False
+        assert u.id == int(u.get_id())
+
+    def test_make_unique_nickname(self):
+        # create a user and write it to the database
+        u = User(nickname = 'john', email = 'john@example.com')
+        db.session.add(u)
+        db.session.commit()
+        nickname = User.make_unique_nickname('susan')
+        assert nickname == 'susan'
+        nickname = User.make_unique_nickname('john')
+        assert nickname != 'john'
+    
 if __name__ == '__main__':
-    unittest.main()
+    try:
+        unittest.main()
+    except:
+        pass
+    cov.stop()
+    cov.save()
+    print '\n\nCoverage Report:\n'
+    cov.report()
+    print 'HTML version: ', os.path.join(basedir, 'tmp/coverage/index.html')
+    cov.html_report(directory = 'tmp/coverage')
+    cov.erase()
